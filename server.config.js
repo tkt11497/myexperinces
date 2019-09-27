00bb68b42403e8f7c76bc6ce0a18ade9bd9df455
Aung Kyaw Kyaw Myo <aungkyawkyawmyo@marathonmyanmar.com>
2019-09-27 17:53:11 +0630

    change pm2 server port

--- server.config.js
+++ server.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-    apps: [{
-        name: 'Marathon Myanmar Dashboard',
-        script: './server/index.js',
-        instances: 0,
-        exec_mode: 'cluster',
-        watch: true,
-        env: {
-            NODE_ENV: 'production',
-            PORT: '8000'
-        }
-    }]
+  apps: [
+    {
+      name: "Marathon Myanmar Dashboard",
+      script: "./server/index.js",
+      instances: 0,
+      exec_mode: "cluster",
+      watch: true,
+      env: {
+        NODE_ENV: "production",
+        PORT: "9000"
+      }
+    }
+  ]
 };
