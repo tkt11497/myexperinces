6fca8e59dfe13f2d312d19dbc73c4c02cd0d70c3
Aung Kyaw Kyaw Myo <aungkyawkyawmyo@marathonmyanmar.com>
2019-09-27 18:18:58 +0630

    change pm2 server port

--- server.config.js
+++ server.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "Marathon Myanmar Dashboard",
+      name: "Merchant Dashboard",
       script: "./server/index.js",
       instances: 0,
       exec_mode: "cluster",
